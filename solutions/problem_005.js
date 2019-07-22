6bcd74b3fa161153b0e123d2f939b7b1f185c532
Adam Espinoza <adamespi@usc.edu>
2019-07-22 09:40:52 -0700

    Updated solution to problem_005.js

---
+++ solutions/problem_005.js
@@ -0,0 +1,21 @@
+const cons = (a, b) => {
+  const pair = (f) => {
+    return f(a,b);
+  }
+  return pair;
+}
+
+const car = (pair) => {
+  const f = (a,b) => { return a; }
+  return pair(f);
+}
+
+const cdr = (pair) => {
+  const f = (a,b) => { return b;}
+  return pair(f);
+}
+
+console.log(car(cons(3, 4)));  // 3
+console.log(cdr(cons(3, 4)));  // 4
+console.log(car(cons(4, 3)));  // 4
+console.log(cdr(cons(4, 3)));  // 3
