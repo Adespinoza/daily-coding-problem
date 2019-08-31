c27baa04be91f5b040f12103addb07563aed8b20
Adam Espinoza <adamespi@usc.edu>
2019-08-31 17:37:48 -0500

    Updated solution to problem_045.js

---
+++ solutions/problem_045.js
@@ -0,0 +1,24 @@
+/**
+ * Returns an integer from 1 to 5 (inclusive) with uniform probability
+ * @return {number}
+ */
+function rand5() {
+  return Math.floor(Math.random() * 5) + 1;
+}
+
+/**
+ * Returns an integer from 1 to 25 using rand5
+ */
+function rand25() {
+  return 5 * (rand5() - 1) + (rand5() - 1);
+}
+
+/**
+ * Returns an integer from 1 to 7 (inclusive) using rand5
+ * @return {number}
+ */
+function rand7() {
+  let r = 21;
+  while (r >= 21) r = rand25();
+  return (r % 7) + 1;
+}
