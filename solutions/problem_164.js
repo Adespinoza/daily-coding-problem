11423332e490872a409cae8d8ca77ef8d1bc4ef6
Adam Espinoza <adamespi@usc.edu>
2019-12-28 22:23:33 -0800

    Updated solution to problem_164.js

---
+++ solutions/problem_164.js
@@ -0,0 +1,18 @@
+/**
+ * [findDuplicate description]
+ * @param  {number[]} arr
+ * @return {number}
+ */
+function findDuplicate(arr) {
+  const map = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (map.has(arr[i])) return arr[i];
+    map.set(arr[i], arr[i]);
+  }
+
+  return null;
+}
+
+console.log(findDuplicate([69, 1, 69, 6, 100, 4])); // 69
+console.log(findDuplicate([1, 2, 3, 3, 4, 5])); // 3
