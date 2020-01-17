3a6f129e2490d782b16ece1f8f84867b003375ec
Adam Espinoza <adamespi@usc.edu>
2020-01-17 15:18:16 -0800

    Updated solution to problem_183.js

---
+++ solutions/problem_183.js
@@ -0,0 +1,6 @@
+// Describe what happens when you type a URL into your browser and press Enter.
+// Check browsers host resolution cache.
+// If IP not found, request DNS for the IP of the host/load balancing host.
+// Send a GET request to the host
+// Host responds with a payload (usually in HTML)
+// Browser renders HTML
