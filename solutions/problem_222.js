75c419be9605e97603e0f8f65136948bf984e4e2
Adam Espinoza <adamespi97@gmail.com>
2020-02-24 22:01:22 -0800

    Updated solution to problem_222.js

---
+++ solutions/problem_222.js
@@ -0,0 +1,20 @@
+/**
+ * Find shortest standardized path given an absolute pathname
+ * @param {string} path
+ */
+function simplifyPath(path) {
+  const stack = [];
+  path = path.split('/');
+
+  for (let i = 0; i < path.length; i++) {
+    if (path[i] === '.' || path[i] === '') continue;
+    if (path[i] === '..') stack.pop();
+    else stack.push(path[i]);
+  }
+
+  return `/${stack.join('/')}`;
+}
+
+console.log(simplifyPath('/usr/bin/../bin/./scripts/../')); // '/usr/bin/'
+console.log(simplifyPath('/a//b////c/d//././/..')); // '/a/b/c'
+console.log(simplifyPath('/a/./b/../../c/')); // '/c'
